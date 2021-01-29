9aa82847a36e489bef504a8caee9198ea15eb51b
Giuseppe Piccininni <piccininnig5@gmail.com>
2021-01-29 13:22:54 +0100

    GPS Feature

--- app/src/main/java/com/example/alessandroorlando/MainActivity.java
+++ app/src/main/java/com/example/alessandroorlando/MainActivity.java
@@ -1,28 +1,37 @@
 package com.example.alessandroorlando;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
-    private MediaPlayer voi, mettilo, figlio, agitato, buio;
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+    private MediaPlayer voi, mettilo, figlio, agitato, buio, dove;
     private Button voi_btn;
     private Button mettilo_btn;
     private Button figlio_btn;
     private Button buio_btn;
+    private Button dove_btn;
     private boolean isAccelerometerAvailable;
     private SensorManager sensorManager;
     private Sensor accelerometerSensor;
+    private Intent intentGPS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +43,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         figlio = MediaPlayer.create(this, R.raw.figlio);
         agitato = MediaPlayer.create(this, R.raw.agitato);
         buio = MediaPlayer.create(this, R.raw.buio);
+        dove = MediaPlayer.create(this, R.raw.dove);
 
         voi_btn = (Button) this.findViewById(R.id.voi_btn);
         mettilo_btn = (Button) this.findViewById(R.id.da_parte_btn);
         figlio_btn = (Button) this.findViewById((R.id.figlio_btn));
         buio_btn = (Button) this.findViewById((R.id.buio_btn));
+        dove_btn = (Button) this.findViewById((R.id.dove_btn));
 
         voi_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,9 +77,31 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             public void onClick(View v) {
                 buio.start();
                 WindowManager.LayoutParams params = getWindow().getAttributes();
-                params.flags |= WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON;
+                params.flags = WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON;
                 params.screenBrightness=0;
                 getWindow().setAttributes(params);
+                buio.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        //buio.stop();
+                        WindowManager.LayoutParams params = getWindow().getAttributes();
+                        params.flags = WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON;
+                        params.screenBrightness=200;
+                        getWindow().setAttributes(params);
+                    }
+                });
+            }
+        });
+
+        dove_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isLocationEnabled(MainActivity.this)){
+                    dove.start();
+                } else {
+                    intentGPS = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivity(intentGPS);
+                }
             }
         });
 
@@ -118,5 +151,51 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         if (isAccelerometerAvailable){
             sensorManager.unregisterListener(this);
         }
+    }
+
+    public static boolean isLocationEnabled(Context context) {
+        int locationMode = 0;
+        String locationProviders;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+            try {
+                locationMode = Settings.Secure.getInt(context.getContentResolver(), Settings.Secure.LOCATION_MODE);
+
+            } catch (Settings.SettingNotFoundException e) {
+                e.printStackTrace();
+                return false;
+            }
+
+            return locationMode != Settings.Secure.LOCATION_MODE_OFF;
+
+        }else{
+            locationProviders = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+            return !TextUtils.isEmpty(locationProviders);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+        CloseAlert("VUOI DAVVERO ALLONTANARTI DAL MAESTRO?");
+    }
+
+    public void CloseAlert(String alertmessage){
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which){
+                    case DialogInterface.BUTTON_POSITIVE:
+                        android.os.Process.killProcess(android.os.Process.myPid());
+                        break;
+
+                        case DialogInterface.BUTTON_NEGATIVE:
+                            break;
+                }
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(alertmessage).setPositiveButton("Sì", dialogClickListener).setNegativeButton("No", dialogClickListener).show();
     }
 }
